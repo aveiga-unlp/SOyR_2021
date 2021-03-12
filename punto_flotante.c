687957fb4abd2d42dc22558ef3c43007a6640567
aveiga-unlp <80488509+aveiga-unlp@users.noreply.github.com>
2021-03-12 18:21:33 -0300

    Create punto_flotante.c

---
+++ punto_flotante.c
@@ -0,0 +1,31 @@
+// Punto flotante
+
+// a) Intente predecir la salida
+// b) Compile y verifique
+// c) Calcule el error en la representación (double mejora?)
+
+// Solución: -pi
+// 0xC0490FDB = 0b11000000010010010000111111011011  little endian
+//                Seeeeeeeemmmmmmmmmmmmmmmmmmmmmmm
+//             = -11.0010010000111111011011  (sólo 25 dígitos)
+
+// Corolario: La representación simbólica no tiene error y
+// ocupa sólo 2 bytes ASCII   :-)
+
+#include<stdio.h>
+#include <math.h>
+
+void main() {
+
+    float x;
+    int y;
+    char* p = "pi";
+
+    x = -4 * atan(1.0);
+    y = *(int*)&x;           // Pista: &x obtiene la dirección, (int*) apunta a la dirección como entero, * obtiene lo apuntado
+
+    printf("x = %.9f %d 0x%08X %s %2X %2X\n", x, y, y, p, p[0], p[1]);
+
+}
+
+
