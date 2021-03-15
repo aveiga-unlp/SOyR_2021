d3090d99c4782903b0230537325f0e7e9ada7c1a
aveiga-unlp <alejandro.veiga@gmail.com>
2021-03-15 18:48:54 -0300

    Desde new

--- punto_flotante.c
+++ punto_flotante.c
@@ -2,7 +2,9 @@
 // A. Veiga, SOyR 2021
 // a) Intente predecir la salida
 // b) Compile y verifique que su predicción sea correcta
-// c) Calcule el error en la representación (double mejora?)
+// c) Calcule el error en la representación. Utilizando double mejora?
+// d) Proponga un formato para almacenar en 32 bits el número PI con la mayor 
+//    cantidad de dígitos. Calcule er error.
 
 #include <stdio.h>
 #include <math.h>
@@ -11,23 +13,26 @@ void main() {
 
     float x;
     int y;
-    char* p = "-pi";
+    char* p = "pi";
 
-    x = -4 * atan(1.0);
-    y = *(int*)&x;          // Pista: &x obtiene la dirección, (int*) apunta a
+    x = 4 * atan(1.0);		// Una forma ingeniosa de obtener el número PI
+    y = *(int*)&x;          // Truco para imprimir un float en hex
+    						// Pista: &x obtiene la dirección, (int*) apunta a
                             // la dirección como entero, * obtiene lo apuntado
-                            // Truco para imprimir un float en hex
+                            
 
-    printf("x = %.9f %d 0x%08X %s %2X %2X %2X\n", x, y, y, p, p[0], p[1], p[2]);
+    printf("x = %.9f %d 0x%08X %s %2X %2X\n", x, y, y, p, p[0], p[1]);
 
 }
 
 //
 // Solución: -pi
-// 0xC0490FDB = 0b11000000010010010000111111011011  little endian
+// 0x40490FDB = 0b01000000010010010000111111011011  little endian
 //                Seeeeeeeemmmmmmmmmmmmmmmmmmmmmmm
-//             = -11.0010010000111111011011  (sólo 25 dígitos de resolución)
+//             = 11.0010010000111111011011  (sólo 25 dígitos en 32)
 //
 // Corolario:   La representación simbólica no tiene error
 //              Y ocupa sólo 3 bytes ASCII   :-)
+//
+// En punto fijo: 2 de entero, 30 de decimales
 //
