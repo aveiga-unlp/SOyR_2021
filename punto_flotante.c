0f69469bc06012ed1f5ab63d71ef3e48abe8915d
aveiga-unlp <alejandro.veiga@gmail.com>
2021-03-15 13:21:12 -0300

    Test desde lubuntu 2

--- punto_flotante.c
+++ punto_flotante.c
@@ -1,4 +1,4 @@
-// Punto flotante 2021 newwwww
+// Punto flotante 2021
 
 // a) Intente predecir la salida
 // b) Compile y verifique
@@ -7,12 +7,12 @@
 // Solución: -pi
 // 0xC0490FDB = 0b11000000010010010000111111011011  little endian
 //                Seeeeeeeemmmmmmmmmmmmmmmmmmmmmmm
-//             = -11.0010010000111111011011  (sólo 25 dígitos)
+//             = -11.0010010000111111011011  (sólo 25 dígitos de resolución)
 
 // Corolario: La representación simbólica no tiene error y
 // ocupa sólo 2 bytes ASCII   :-)
 
-#include<stdio.h>
+#include <stdio.h>
 #include <math.h>
 
 void main() {
@@ -22,7 +22,9 @@ void main() {
     char* p = "pi";
 
     x = -4 * atan(1.0);
-    y = *(int*)&x;           // Pista: &x obtiene la dirección, (int*) apunta a la dirección como entero, * obtiene lo apuntado
+    y = *(int*)&x;          // Pista: &x obtiene la dirección, (int*) apunta a
+                            // la dirección como entero, * obtiene lo apuntado
+                            // Truco para imprimir un float en hex
 
     printf("x = %.9f %d 0x%08X %s %2X %2X\n", x, y, y, p, p[0], p[1]);
 
